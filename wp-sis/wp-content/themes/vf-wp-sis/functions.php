72078848ee21d0ab7e3884a25b4a7f4d8ee43fbd
Alexander Kubias <mail@alexander-kubias.de>
2021-02-18 11:46:17 +0100

    Add ELLS theme and small changes for SIS theme.

---
+++ wp-sis/wp-content/themes/vf-wp-sis/functions.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once('functions/sis-article-post.php');
+
+require_once('functions/custom-taxonomies.php');
+
+
+require_once('functions/ells-breadcrumbs.php');
+require_once('functions/learning-labs-post.php');
+require_once('functions/teachingbase-post.php');
+require_once('functions/embl-visit-post.php');
+require_once('functions/insight-lecture-post.php');
+
+// enable featured image
+add_theme_support('post-thumbnails');
+add_theme_support('title-tag');
+
+
+// CHILD THEME CSS FILE
+add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
+function my_theme_enqueue_styles()
+{
+
+  $parent_style = 'parent-style';
+
+  wp_enqueue_style($parent_style, get_template_directory_uri() . '/style.css');
+  wp_enqueue_style('child-style',
+    get_stylesheet_directory_uri() . '/style.css',
+    array($parent_style),
+    wp_get_theme()->get('Version')
+  );
+}
+
---
+++ wp-sis/wp-content/themes/vf-wp-sis/index.php
@@ -0,0 +1,49 @@
+<?php
+
+get_header();
+
+?>
+<section class="vf-hero vf-hero--primary vf-hero--block vf-hero--800 | vf-u-fullbleed | vf-u-margin__bottom--0" style="--vf-hero--bg-image: url('https://acxngcvroo.cloudimg.io/v7/https://www.embl.org/files/wp-content/uploads/Ells_Masthead_1000x600.png');  ">
+  <div class="vf-hero__content | vf-stack vf-stack--400 ">
+    <h2 class="vf-hero__heading">
+      SIS
+    </h2>
+    <p class="vf-hero__subheading">Science in School</p>
+  </div>
+</section>
+<?php
+
+if (class_exists('VF_Navigation')) {
+  VF_Plugin::render(VF_Navigation::get_plugin('vf_navigation'));
+}
+
+?>
+
+<section class="vf-grid vf-grid__col-3 | vf-content">
+    <div class="vf-grid__col--span-2">
+        <h2>
+          Latest news</h2>
+      <div>
+        <?php $page = (get_query_var('paged')) ? get_query_var('paged') : 1;
+$args = array(
+    'posts_per_page' => 6,
+    'paged' => $page,);
+query_posts($args);?>
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+        <?php include(locate_template('partials/vf-summary--news.php', false, false)); ?>
+        <?php endwhile; endif; ?>
+      </div>
+      <div class="vf-grid" style="margin: 4%"> <?php vf_pagination();
+      ?>
+      </div>
+      </div>
+      <div>
+
+
+      </div>
+
+
+</section>
+
+
+<?php get_footer(); ?>
