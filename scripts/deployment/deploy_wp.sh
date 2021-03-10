1bd1f9c2e23756cd8d7d69fd103b71c441adff68
Alexander Kubias <mail@alexander-kubias.de>
2021-03-10 13:42:54 +0100

    Add scripts for deployment and backup.

--- scripts/deployment/deploy_wp.sh
+++ scripts/deployment/deploy_wp.sh
@@ -48,10 +48,21 @@ rm -f wp-signup.php
 rm -f wp-trackback.php
 rm -f xmlrpc.php
 
+cd /var/www/html/wordpress/wp-content/themes/
+rm -rf ./vf-wp/
+rm -rf ./vf-wp-documents/
+rm -rf ./vf-wp-ells/
+rm -rf ./vf-wp-elmi/
+rm -rf ./vf-wp-groups/
+rm -rf ./vf-wp-intranet/
+rm -rf ./vf-wp-sis/
+
+
 cd /var/www/webdeploy/science-in-school/deployable/wordpress/
 cp -R ./wp-admin/ /var/www/html/wordpress/
 cp -R ./wp-includes/ /var/www/html/wordpress/
 
+cp index.php /var/www/html/wordpress/
 cp wp-activate.php /var/www/html/wordpress/
 cp wp-blog-header.php /var/www/html/wordpress/
 cp wp-comments-post.php /var/www/html/wordpress/
@@ -64,6 +75,18 @@ cp wp-settings.php /var/www/html/wordpress/
 cp wp-signup.php /var/www/html/wordpress/
 cp wp-trackback.php /var/www/html/wordpress/
 cp xmlrpc.php /var/www/html/wordpress/
+
+
+cd /var/www/webdeploy/science-in-school/deployable/vf-wp/wp-content/themes/
+cp -R ./vf-wp/ /var/www/html/wordpress/wp-content/themes/
+cp -R ./vf-wp-documents/ /var/www/html/wordpress/wp-content/themes/
+cp -R ./vf-wp-ells/ /var/www/html/wordpress/wp-content/themes/
+cp -R ./vf-wp-elmi/ /var/www/html/wordpress/wp-content/themes/
+cp -R ./vf-wp-groups/ /var/www/html/wordpress/wp-content/themes/
+cp -R ./vf-wp-intranet/ /var/www/html/wordpress/wp-content/themes/
+
+cd /var/www/webdeploy/science-in-school/deployable/vf-wp/wp-content/themes/
+cp -R ./vf-wp-sis/ /var/www/html/wordpress/wp-content/themes/
 
 chown -R apache:apache /var/www/html/wordpress
 
