bbaa6217039009e095c013b7f59de38377af33b5
Alexander Kubias <mail@alexander-kubias.de>
2021-03-10 13:48:31 +0100

    Add scripts for deployment and backup.

--- scripts/deployment/deploy_wp_copy.sh
+++ scripts/deployment/deploy_wp_copy.sh
@@ -5,29 +5,6 @@ set -e;
 set -u;
 
 SCRIPTS_DEPLOY_DIR=$(pwd -P);
-WP_VERSION=latest
-
-cd ../../deployable/
-
-rm -rf ./wp.tar.gz
-rm -rf ./wordpress
-curl -o wp.tar.gz https://wordpress.org/${WP_VERSION}.tar.gz
-
-tar xzvf wp.tar.gz -C ./
-
-rm -rf ./vf-wp
-git clone https://github.com/visual-framework/vf-wp.git
-cd ./vf-wp
-git checkout master
-git pull origin master
-cd ..
-
-rm -rf ./vfwp-external-plugins
-git clone https://github.com/visual-framework/vfwp-external-plugins.git
-cd ./vfwp-external-plugins
-git checkout master
-git pull origin master
-cd ..
 
 # Copy all files to website folder
 cd /var/www/html/wordpress/
