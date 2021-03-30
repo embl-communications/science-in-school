6041cfc37063e9c5f4160be7dbcb9f0355dbd7e1
Alexander Kubias <mail@alexander-kubias.de>
2021-03-30 10:49:26 +0200

    Download script.

---
+++ scripts/deployment/deploy_wp_download.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# IMPORTANT: This script is only allowed to be run on the DEV server!
+
+# halt on errors
+set -e;
+set -u;
+
+SCRIPTS_DEPLOY_DIR=$(pwd -P);
+
+source /var/www/webdeploy/science-in-school/scripts/backup/backup_secrets.sh
+
+# IMPORTANT: You have to run the backup_once.sh script on the BETA server before!
+# Copy all files to website folder
+cd /var/www/backup
+
+# Delete all backup files
+rm -f ./wp_db_once.sql
+rm -f ./wordpress.once.tar.gz
+
+# Copy files from beta to dev.beta
+scp root@sis-web01:/var/www/backup/wp_db_once.sql .
+scp root@sis-web01:/var/www/backup/wordpress.once.tar.gz .
+
+# Delete current tables in database
+TABLES=$($MYSQL -u ${USER} -p${PASS} ${DB} -e 'show tables' | $AWK '{ print $1}' | $GREP -v '^Tables' )
+
+for t in $TABLES
+do
+	echo "Deleting $t table from ${DB} database..."
+	#$MYSQL -u ${USER} -p${PASS} ${DB} -e "drop table $t"
+done
+
+# Insert new data into database
+
+# Delete old wordpress files
+
+# Extract and copy new wordpress files
