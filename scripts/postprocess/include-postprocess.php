378e29e494c65dde57b4eaa93268d99e60db9268
Alexander Kubias <mail@alexander-kubias.de>
2021-04-06 13:36:10 +0200

    Update postprocessing scripts for articles and issues.

---
+++ scripts/postprocess/include-postprocess.php
@@ -0,0 +1,7 @@
+<?php
+// Variables for postprocessing files
+
+// Fix params
+$serverName = "localhost"; // database host
+$dbName = "wordpress";   // WP db name
+$dbNameDrupal = "drupal"; // Drupal db Name
