4dfcede7f78977f2bac3e20c8fa823bac7c2979a
Alexander Kubias <mail@alexander-kubias.de>
2021-04-07 11:42:51 +0200

    Change slug for articles and issues.

--- wp-content/themes/vf-wp-sis/functions/sis-article-post.php
+++ wp-content/themes/vf-wp-sis/functions/sis-article-post.php
@@ -30,7 +30,7 @@ add_action(
       'supports'            => array('title', 'editor', 'page-attributes', 'excerpt', 'thumbnail'),
       'has_archive'         => true,
       'rewrite'             => array(
-        'slug' => 'sis-article'
+        'slug' => ''
       ),
       'query_var'           => true,
       'can_export'          => true,
