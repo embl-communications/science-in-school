10b78a0721133aa9bc5e30543a4ed8cbd503b65b
Alexander Kubias <a.kubias@alperion.de>
2021-07-27 15:50:50 +0200

    Changed events and fix for widget. (#279)
    Co-authored-by: Alexander Kubias <mail@alexander-kubias.de>

--- wp-content/themes/vf-wp-sis/partials/vf-front-webinarsSingleWebinar.php
+++ wp-content/themes/vf-wp-sis/partials/vf-front-webinarsSingleWebinar.php
@@ -3,7 +3,8 @@ $registration_closing = get_field('vf_event_registration_closing');
 $thumbnail_image = get_field('event_addon_thumbnail_image');
 ?>
 <article class="vf-summary vf-summary--news">
-    <span class="vf-summary__date"><?php sis_printFieldWithHeader('Deadline: ', $registration_closing); ?></span>
+    <span class="vf-summary__date"><?php sis_printFieldWithHeader('Registration-Deadline (CET): ', $registration_closing); ?></span>
+    <a href="<?php echo get_the_permalink(); ?>">
     <?php
     if (is_array($thumbnail_image) && array_key_exists('url', $thumbnail_image)) {
         ?>
@@ -13,15 +14,16 @@ $thumbnail_image = get_field('event_addon_thumbnail_image');
         <?php
     }
     ?>
+    </a>
     <h3 class="vf-summary__title">
         <a href="<?php echo get_the_permalink(); ?>" class="vf-summary__link"><?php echo get_the_title(); ?></a>
     </h3>
     <p class="vf-summary__text">
         <?php echo get_the_excerpt(); ?>
         <br/><br/>
-        <?php sis_printFieldWithHeader('Startdate (CET): ', get_field('vf_event_start_date')); ?>
+        <span class="vf-summary__date"><?php sis_printFieldWithHeader('Start date (CET): ', get_field('vf_event_start_date')); ?></span>
         <br/>
-        <?php sis_printFieldWithHeader('Starttime (CET): ', get_field('vf_event_start_time')); ?>
+        <span class="vf-summary__date"><?php sis_printFieldWithHeader('Start time (CET): ', get_field('vf_event_start_time')); ?></span>
 
     </p>
 </article>
