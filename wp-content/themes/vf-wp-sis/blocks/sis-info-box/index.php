e3a859070a7937a4d40790bc213dbb619ea5f7c0
Ken Hawkins <khawkins98@gmail.com>
2021-07-27 12:19:02 +0200

    Feature: info boxes
    This is a frist attempt to add support for custom info boxes.
    
    Notes:
    - I've not tested it on my local (but it should be "fine")
    - Loads the block through the child theme
    - Doesn't do anything special on the front end yet, just adds an extra class (`sis-info-box`)
    - Is the basis for future work
    
    @alexk1204 if you're happy, can you please review this and merge for further work on the dev server?

---
+++ wp-content/themes/vf-wp-sis/blocks/sis-info-box/index.php
@@ -0,0 +1,45 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+if ( ! class_exists('SIS_Info_Box') ) :
+
+// sis-info-box
+
+  require_once('widget.php');
+class SIS_Info_Box extends VFWP_Block {
+
+  public function __construct() {
+    parent::__construct(__FILE__);
+  }
+
+  /**
+   * Return the block name
+   */
+  static public function get_name() {
+    return 'vfwp-box';
+  }
+
+  /**
+   * Return Gutenberg block registration configuration
+   * https://www.advancedcustomfields.com/resources/acf_register_block_type/
+   * https://developer.wordpress.org/block-editor/developers/block-api/block-registration/
+   */
+  public function get_config() {
+    return array(
+      'name'     => $this->get_name(),
+      'title'    => 'Box',
+      'category' => 'vf/wp',
+      'supports' => array(
+        'align'           => false,
+        'customClassName' => false
+      )
+    );
+  }
+
+} // SIS_Info_Box
+
+// Initialize one instance
+$SIS_Info_Box = new SIS_Info_Box();
+
+endif; ?>
---
+++ wp-content/themes/vf-wp-sis/blocks/sis-info-box/template.php
@@ -0,0 +1,106 @@
+<?php
+
+// Block preview in Gutenberg editor
+$is_preview = isset($is_preview) && $is_preview;
+
+$heading = get_field('heading');
+$text = get_field('text');
+$text = wpautop($text);
+$text = str_replace('<p>', '<p class="vf-box__text">', $text);
+$link = get_field('link');
+
+$style = get_field('style');
+if (empty($style)) {
+  $style = 'easy';
+}
+if (is_array($style)) {
+  $style = $style[0];
+}
+
+$theme_easy = get_field('theme_easy');
+if (empty($theme_easy)) {
+  $theme_easy = 'none';
+}
+if (is_array($theme_easy)) {
+  $theme_easy = $theme_easy[0];
+}
+
+$theme_normal = get_field('theme_normal');
+if (empty($theme_normal)) {
+  $theme_normal = 'none';
+}
+if (is_array($theme_normal)) {
+  $theme_normal = $theme_normal[0];
+}
+
+$theme_none = get_field('theme_none');
+if (empty($theme_none)) {
+  $theme_none = 'very-easy';
+}
+if (is_array($theme_none)) {
+  $theme_none = $theme_none[0];
+}
+
+
+// Function to output a banner message in the Gutenberg editor only
+$admin_banner = function($message, $modifier = 'info') use ($is_preview) {
+  if ( ! $is_preview) {
+    return;
+  }
+?>
+<div class="vf-banner vf-banner--alert vf-banner--<?php echo $modifier; ?>">
+  <div class="vf-banner__content">
+    <p class="vf-banner__text">
+      <?php echo $message; ?>
+    </p>
+  </div>
+</div>
+<!--/vf-banner-->
+<?php
+};
+
+if (
+  vf_html_empty($heading)
+  && vf_html_empty($text)
+) {
+  $admin_banner(__('Please enter content for this box.', 'vfwp'));
+  return;
+}
+
+$classes = "vf-box";
+
+if ($link) {
+  $classes .= " vf-box--is-link";
+}
+
+if ($style === 'none') {
+  $classes .= " vf-box--{$theme_none}";
+}
+  
+if ($style === 'normal') {
+$classes .= " vf-box--{$style}";
+if ($style !== 'easy') {
+  $classes .= " vf-box-theme--{$theme_normal}";
+} }
+
+if ($style === 'easy') {
+$classes .= " vf-box--{$style}";
+if ($style !== 'normal') {
+  $classes .= " vf-box-theme--{$theme_easy}";
+} }?>
+
+<div class="<?php echo esc_attr($classes); ?>">
+  <?php if (! empty($heading)) { ?>
+    <h3 class="vf-box__heading">
+      <?php } if ($link) { ?>
+        <a class="vf-box__link" href="<?php echo esc_url($link['url']); ?>">
+          <?php } ?>
+          <?php echo esc_html($heading); ?>
+        <?php if ($link) { ?>
+        </a>
+        <?php }  ?>
+      <?php if (! empty($heading)) { ?>
+    </h3> 
+      <?php } ?>
+  <?php echo ($text); ?>
+</div>
