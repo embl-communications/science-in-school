eddbd0c62598dbce788f1ca149ad0a76eef19263
Alexander Kubias <mail@alexander-kubias.de>
2021-08-24 12:00:27 +0200

    Change links.

--- wp-content/themes/vf-wp-sis/partials/vf-front-contribute.php
+++ wp-content/themes/vf-wp-sis/partials/vf-front-contribute.php
@@ -5,7 +5,7 @@
     </section>
     <section class="vf-grid vf-grid__col-3 | vf-u-margin__top--600">
         <article class="vf-box vf-box--is-link vf-box-theme--tertiary vf-box--normal">
-            <h3 class="vf-box__heading"><a class="vf-box__link" href="/training">Write an article</a></h3>
+            <h3 class="vf-box__heading"><a class="vf-box__link" href="/submit-article">Write an article</a></h3>
             <p class="vf-box__text">
                 Do you have an engaging classroom activity to share with other teachers? Is there an interesting
                 scientific topic that you could explain to STEM teachers and their students? We welcome submissions
@@ -14,7 +14,7 @@
         </article>
 
         <article class="vf-box vf-box--is-link vf-box-theme--tertiary vf-box--normal">
-            <h3 class="vf-box__heading"><a class="vf-box__link" href="/training">Review an article</a></h3>
+            <h3 class="vf-box__heading"><a class="vf-box__link" href="/review-articles">Review an article</a></h3>
             <p class="vf-box__text">
                 Would you like to help ensure that our content is interesting, inspiring and useful to STEM teachers?
                 Consider joining the Science in School teacher reviewer panel. There is no obligation; just send us an
@@ -23,7 +23,7 @@
         </article>
 
         <article class="vf-box vf-box--is-link vf-box-theme--tertiary vf-box--normal">
-            <h3 class="vf-box__heading"><a class="vf-box__link" href="/training">Translate an article</a></h3>
+            <h3 class="vf-box__heading"><a class="vf-box__link" href="/translate-articles">Translate an article</a></h3>
             <p class="vf-box__text">
                 If you find an article interesting or useful, perhaps you'd consider translating it into your native
                 language? This really helps to increase the reach of our content so that as many teachers as possible
