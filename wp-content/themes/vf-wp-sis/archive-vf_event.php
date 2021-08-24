a5d44a857b8da9b4d8062cd1928eb1fc0c507c2a
Alexander Kubias <mail@alexander-kubias.de>
2021-08-24 11:48:09 +0200

    Small bugfixes.

--- wp-content/themes/vf-wp-sis/archive-vf_event.php
+++ wp-content/themes/vf-wp-sis/archive-vf_event.php
@@ -51,7 +51,8 @@ get_header();
         <div class="vf-section-header"><a class="vf-section-header__heading vf-section-header__heading--is-link" href="/events/" id="section-sub-heading-link-text">EIROforum events <svg aria-hidden="true" class="vf-section-header__icon | vf-icon vf-icon-arrow--inline-end" width="1em" height="1em" xmlns="http://www.w3.org/2000/svg">
                     <path d="M0 12c0 6.627 5.373 12 12 12s12-5.373 12-12S18.627 0 12 0C5.376.008.008 5.376 0 12zm13.707-5.209l4.5 4.5a1 1 0 010 1.414l-4.5 4.5a1 1 0 01-1.414-1.414l2.366-2.367a.25.25 0 00-.177-.424H6a1 1 0 010-2h8.482a.25.25 0 00.177-.427l-2.366-2.368a1 1 0 011.414-1.414z" fill="" fill-rule="nonzero"></path>
                 </svg></a>
-            <p class="vf-section-header__text">Hosted by EIROforum members.</p>
+            <p class="vf-section-header__text">Find out about up-coming Science in School webinars,
+                which are hosted by different EIROforum member institutions.</p>
         </div>
         <div class="vf-news-container__content vf-grid vf-grid__col-3">
             <?php
