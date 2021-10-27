acbc4a060b3c8391bffba2ecf6011588dbbe1355
Szymon Kasprzyk <szymon.kasprzyk@embl.de>
2021-10-27 21:39:28 +0200

    adds instagram to the footer

--- dist/wp-content/themes/vf-wp-sis/partials/vf-footer.php
+++ dist/wp-content/themes/vf-wp-sis/partials/vf-footer.php
@@ -1,7 +1,8 @@
 <footer class="vf-footer">
     <div class="vf-footer__inner">
         <p class="vf-footer__notice">
-            Supporting STEM teachers in inspiring their students and fostering positive attitudes towards the science that shapes our lives.
+            Supporting STEM teachers in inspiring their students and fostering positive attitudes towards the science
+            that shapes our lives.
         </p>
 
         <section class="vf-footer__links-group | vf-grid">
@@ -50,26 +51,26 @@
                     <defs>
                         <g id="vf-social--linkedin">
                             <rect xmlns="http://www.w3.org/2000/svg" width="5" height="14" x="2" y="8.5" rx=".5"
-                                  ry=".5"/>
-                            <ellipse xmlns="http://www.w3.org/2000/svg" cx="4.48" cy="4" rx="2.48" ry="2.5"/>
+                                ry=".5" />
+                            <ellipse xmlns="http://www.w3.org/2000/svg" cx="4.48" cy="4" rx="2.48" ry="2.5" />
                             <path xmlns="http://www.w3.org/2000/svg"
-                                  d="M18.5,22.5h3A.5.5,0,0,0,22,22V13.6C22,9.83,19.87,8,16.89,8a4.21,4.21,0,0,0-3.17,1.27A.41.41,0,0,1,13,9a.5.5,0,0,0-.5-.5h-3A.5.5,0,0,0,9,9V22a.5.5,0,0,0,.5.5h3A.5.5,0,0,0,13,22V14.5a2.5,2.5,0,0,1,5,0V22A.5.5,0,0,0,18.5,22.5Z"/>
+                                d="M18.5,22.5h3A.5.5,0,0,0,22,22V13.6C22,9.83,19.87,8,16.89,8a4.21,4.21,0,0,0-3.17,1.27A.41.41,0,0,1,13,9a.5.5,0,0,0-.5-.5h-3A.5.5,0,0,0,9,9V22a.5.5,0,0,0,.5.5h3A.5.5,0,0,0,13,22V14.5a2.5,2.5,0,0,1,5,0V22A.5.5,0,0,0,18.5,22.5Z" />
                         </g>
                         <g id="vf-social--facebook">
                             <path xmlns="http://www.w3.org/2000/svg"
-                                  d="m18.14 7.17a.5.5 0 0 0 -.37-.17h-3.77v-1.41c0-.28.06-.6.51-.6h3a.44.44 0 0 0 .35-.15.5.5 0 0 0 .14-.34v-4a.5.5 0 0 0 -.5-.5h-4.33c-4.8 0-5.17 4.1-5.17 5.35v1.65h-2.5a.5.5 0 0 0 -.5.5v4a.5.5 0 0 0 .5.5h2.5v11.5a.5.5 0 0 0 .5.5h5a.5.5 0 0 0 .5-.5v-11.5h3.35a.5.5 0 0 0 .5-.45l.42-4a.5.5 0 0 0 -.13-.38z"/>
+                                d="m18.14 7.17a.5.5 0 0 0 -.37-.17h-3.77v-1.41c0-.28.06-.6.51-.6h3a.44.44 0 0 0 .35-.15.5.5 0 0 0 .14-.34v-4a.5.5 0 0 0 -.5-.5h-4.33c-4.8 0-5.17 4.1-5.17 5.35v1.65h-2.5a.5.5 0 0 0 -.5.5v4a.5.5 0 0 0 .5.5h2.5v11.5a.5.5 0 0 0 .5.5h5a.5.5 0 0 0 .5-.5v-11.5h3.35a.5.5 0 0 0 .5-.45l.42-4a.5.5 0 0 0 -.13-.38z" />
                         </g>
                         <g id="vf-social--twitter">
                             <path xmlns="http://www.w3.org/2000/svg"
-                                  d="M23.32,6.44a.5.5,0,0,0-.2-.87l-.79-.2A.5.5,0,0,1,22,4.67l.44-.89a.5.5,0,0,0-.58-.7l-2,.56a.5.5,0,0,1-.44-.08,5,5,0,0,0-3-1,5,5,0,0,0-5,5v.36a.25.25,0,0,1-.22.25c-2.81.33-5.5-1.1-8.4-4.44a.51.51,0,0,0-.51-.15A.5.5,0,0,0,2,4a7.58,7.58,0,0,0,.46,4.92.25.25,0,0,1-.26.36L1.08,9.06a.5.5,0,0,0-.57.59,5.15,5.15,0,0,0,2.37,3.78.25.25,0,0,1,0,.45l-.53.21a.5.5,0,0,0-.26.69,4.36,4.36,0,0,0,3.2,2.48.25.25,0,0,1,0,.47A10.94,10.94,0,0,1,1,18.56a.5.5,0,0,0-.2,1,20.06,20.06,0,0,0,8.14,1.93,12.58,12.58,0,0,0,7-2A12.5,12.5,0,0,0,21.5,9.06V8.19a.5.5,0,0,1,.18-.38Z"/>
+                                d="M23.32,6.44a.5.5,0,0,0-.2-.87l-.79-.2A.5.5,0,0,1,22,4.67l.44-.89a.5.5,0,0,0-.58-.7l-2,.56a.5.5,0,0,1-.44-.08,5,5,0,0,0-3-1,5,5,0,0,0-5,5v.36a.25.25,0,0,1-.22.25c-2.81.33-5.5-1.1-8.4-4.44a.51.51,0,0,0-.51-.15A.5.5,0,0,0,2,4a7.58,7.58,0,0,0,.46,4.92.25.25,0,0,1-.26.36L1.08,9.06a.5.5,0,0,0-.57.59,5.15,5.15,0,0,0,2.37,3.78.25.25,0,0,1,0,.45l-.53.21a.5.5,0,0,0-.26.69,4.36,4.36,0,0,0,3.2,2.48.25.25,0,0,1,0,.47A10.94,10.94,0,0,1,1,18.56a.5.5,0,0,0-.2,1,20.06,20.06,0,0,0,8.14,1.93,12.58,12.58,0,0,0,7-2A12.5,12.5,0,0,0,21.5,9.06V8.19a.5.5,0,0,1,.18-.38Z" />
                         </g>
                         <g id="vf-social--youtube">
                             <path xmlns="http://www.w3.org/2000/svg"
-                                  d="M20.06,3.5H3.94A3.94,3.94,0,0,0,0,7.44v9.12A3.94,3.94,0,0,0,3.94,20.5H20.06A3.94,3.94,0,0,0,24,16.56V7.44A3.94,3.94,0,0,0,20.06,3.5ZM16.54,12,9.77,16.36A.5.5,0,0,1,9,15.94V7.28a.5.5,0,0,1,.77-.42l6.77,4.33a.5.5,0,0,1,0,.84Z"/>
+                                d="M20.06,3.5H3.94A3.94,3.94,0,0,0,0,7.44v9.12A3.94,3.94,0,0,0,3.94,20.5H20.06A3.94,3.94,0,0,0,24,16.56V7.44A3.94,3.94,0,0,0,20.06,3.5ZM16.54,12,9.77,16.36A.5.5,0,0,1,9,15.94V7.28a.5.5,0,0,1,.77-.42l6.77,4.33a.5.5,0,0,1,0,.84Z" />
                         </g>
                         <g id="vf-social--instagram">
                             <path xmlns="http://www.w3.org/2000/svg"
-                                  d="M17.5,0H6.5A6.51,6.51,0,0,0,0,6.5v11A6.51,6.51,0,0,0,6.5,24h11A6.51,6.51,0,0,0,24,17.5V6.5A6.51,6.51,0,0,0,17.5,0ZM12,17.5A5.5,5.5,0,1,1,17.5,12,5.5,5.5,0,0,1,12,17.5Zm6.5-11A1.5,1.5,0,1,1,20,5,1.5,1.5,0,0,1,18.5,6.5Z"/>
+                                d="M17.5,0H6.5A6.51,6.51,0,0,0,0,6.5v11A6.51,6.51,0,0,0,6.5,24h11A6.51,6.51,0,0,0,24,17.5V6.5A6.51,6.51,0,0,0,17.5,0ZM12,17.5A5.5,5.5,0,1,1,17.5,12,5.5,5.5,0,0,1,12,17.5Zm6.5-11A1.5,1.5,0,1,1,20,5,1.5,1.5,0,0,1,18.5,6.5Z" />
                         </g>
                     </defs>
                 </svg>
@@ -77,31 +78,44 @@
                     <ul class="vf-social-links__list">
                         <li class="vf-social-links__item">
                             <a class="vf-social-links__link" target="_blank" href="https://twitter.com/SciInSchool">
-                <span class="vf-u-sr-only">
-                  twitter
-                </span>
-                                <svg aria-hidden="true" class="vf-icon vf-icon--social vf-icon--twitter"
-                                     width="24" height="24"
-                                     viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" version="1.1"
-                                     preserveAspectRatio="xMinYMin">
+                                <span class="vf-u-sr-only">
+                                    twitter
+                                </span>
+                                <svg aria-hidden="true" class="vf-icon vf-icon--social vf-icon--twitter" width="24"
+                                    height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" version="1.1"
+                                    preserveAspectRatio="xMinYMin">
                                     <use xlink:href="#vf-social--twitter">
                                     </use>
                                 </svg>
                             </a>
                         </li>
                         <li class="vf-social-links__item">
-                            <a class="vf-social-links__link" target="_blank" href="https://www.facebook.com/scienceinschool">
-                <span class="vf-u-sr-only">
-                  facebook
-                </span>
-                                <svg aria-hidden="true" class="vf-icon vf-icon--social vf-icon--facebook"
-                                     width="24" height="24"
-                                     viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" version="1.1"
-                                     preserveAspectRatio="xMinYMin">
+                            <a class="vf-social-links__link" target="_blank"
+                                href="https://www.facebook.com/scienceinschool">
+                                <span class="vf-u-sr-only">
+                                    facebook
+                                </span>
+                                <svg aria-hidden="true" class="vf-icon vf-icon--social vf-icon--facebook" width="24"
+                                    height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" version="1.1"
+                                    preserveAspectRatio="xMinYMin">
                                     <use xlink:href="#vf-social--facebook">
                                     </use>
                                 </svg>
                             </a>
+                        </li>
+                        <li class="vf-social-links__item">
+
+                            <a class="vf-social-links__link" href="https://www.instagram.com/scienceinschool/">
+                                <span class="vf-u-sr-only">instagram</span>
+
+                                <svg aria-hidden="true" class="vf-icon vf-icon--social vf-icon--instagram" width="24"
+                                    height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" version="1.1"
+                                    preserveAspectRatio="xMinYMin">
+                                    <use xlink:href="#vf-social--instagram"></use>
+                                </svg>
+
+                            </a>
+
                         </li>
                     </ul>
                 </div>
@@ -110,52 +124,65 @@
         <section class="vf-grid vf-cluster" style="--vf-cluster-alignment: center; --vf-cluster__item--flex: 25% 1 0;">
             <div class="sis-sponsor-logo">
                 <a target="_blank" href="https://www.eiroforum.org/">
-                    <img src="/wp-content/themes/vf-wp-sis/assets/images/footer/eiroforum_logo.png" alt="EIROforum" title="EIROforum" loading="lazy">
+                    <img src="/wp-content/themes/vf-wp-sis/assets/images/footer/eiroforum_logo.png" alt="EIROforum"
+                        title="EIROforum" loading="lazy">
                 </a>
             </div>
             <div class="sis-sponsor-logo">
                 <a target="_blank" href="http://www.cern.ch/">
-                    <img src="https://acxngcvroo.cloudimg.io/v7/https://www.embl.org/files/wp-content/uploads/logo-cern.png?width=80" width="40" alt="CERN" title="CERN" loading="lazy">
+                    <img src="https://acxngcvroo.cloudimg.io/v7/https://www.embl.org/files/wp-content/uploads/logo-cern.png?width=80"
+                        width="40" alt="CERN" title="CERN" loading="lazy">
                 </a>
             </div>
             <div class="sis-sponsor-logo">
                 <a target="_blank" href="http://www.embl.org/">
-                    <img src="https://www.embl.org/assets/embl-logo/assets/logo.svg" alt="European Molecular Biology Laboratory" title="European Molecular Biology Laboratory" loading="lazy" width="70">
+                    <img src="https://www.embl.org/assets/embl-logo/assets/logo.svg"
+                        alt="European Molecular Biology Laboratory" title="European Molecular Biology Laboratory"
+                        loading="lazy" width="70">
                 </a>
             </div>
             <div class="sis-sponsor-logo">
                 <a target="_blank" href="http://www.esa.int/">
-                    <img src="https://acxngcvroo.cloudimg.io/v7/https://www.embl.org/files/wp-content/uploads/logo-esa.png?width=140" width="70" alt="European Space Agency" title="European Space Agency" loading="lazy">
+                    <img src="https://acxngcvroo.cloudimg.io/v7/https://www.embl.org/files/wp-content/uploads/logo-esa.png?width=140"
+                        width="70" alt="European Space Agency" title="European Space Agency" loading="lazy">
                 </a>
             </div>
             <div class="sis-sponsor-logo">
                 <a target="_blank" href="http://www.eso.org/">
-                    <img src="https://acxngcvroo.cloudimg.io/v7/https://www.embl.org/files/wp-content/uploads/logo-eso.jpeg?width=80" width="40" alt="European Southern Observatory" title="European Southern Observatory" loading="lazy">
+                    <img src="https://acxngcvroo.cloudimg.io/v7/https://www.embl.org/files/wp-content/uploads/logo-eso.jpeg?width=80"
+                        width="40" alt="European Southern Observatory" title="European Southern Observatory"
+                        loading="lazy">
                 </a>
             </div>
             <div class="sis-sponsor-logo">
                 <a target="_blank" href="http://www.esrf.eu/">
-                    <img src="https://acxngcvroo.cloudimg.io/v7/https://www.embl.org/files/wp-content/uploads/logo-esrf.jpeg?width=80" width="40" alt="European Synchrotron Radiation Facility" title="European Synchrotron Radiation Facility" loading="lazy">
+                    <img src="https://acxngcvroo.cloudimg.io/v7/https://www.embl.org/files/wp-content/uploads/logo-esrf.jpeg?width=80"
+                        width="40" alt="European Synchrotron Radiation Facility"
+                        title="European Synchrotron Radiation Facility" loading="lazy">
                 </a>
             </div>
             <div class="sis-sponsor-logo">
                 <a target="_blank" href="http://www.euro-fusion.org">
-                    <img src="https://acxngcvroo.cloudimg.io/v7/https://www.embl.org/files/wp-content/uploads/logo-eurofusion.png?width=160" width="80" alt="EUROfusion" title="EUROfusion" loading="lazy">
+                    <img src="https://acxngcvroo.cloudimg.io/v7/https://www.embl.org/files/wp-content/uploads/logo-eurofusion.png?width=160"
+                        width="80" alt="EUROfusion" title="EUROfusion" loading="lazy">
                 </a>
             </div>
             <div class="sis-sponsor-logo">
                 <a target="_blank" href="http://www.xfel.eu/">
-                    <img src="https://acxngcvroo.cloudimg.io/v7/https://www.embl.org/files/wp-content/uploads/logo-xfel.jpeg?width=80" width="40" alt="European XFEL" title="European XFEL" loading="lazy">
+                    <img src="https://acxngcvroo.cloudimg.io/v7/https://www.embl.org/files/wp-content/uploads/logo-xfel.jpeg?width=80"
+                        width="40" alt="European XFEL" title="European XFEL" loading="lazy">
                 </a>
             </div>
             <div class="sis-sponsor-logo">
                 <a target="_blank" href="http://www.ill.eu/">
-                    <img src="https://acxngcvroo.cloudimg.io/v7/https://www.embl.org/files/wp-content/uploads/logo-ill.png?width=160" width="80" alt="Institut Laue-Langevin" title="Institut Laue-Langevin" loading="lazy">
+                    <img src="https://acxngcvroo.cloudimg.io/v7/https://www.embl.org/files/wp-content/uploads/logo-ill.png?width=160"
+                        width="80" alt="Institut Laue-Langevin" title="Institut Laue-Langevin" loading="lazy">
                 </a>
             </div>
         </section>
         <p class="vf-footer__legal">
-            <span class="vf-footer__legal-text"><a class="vf-footer__link" href="https://www.eiroforum.org/">Published and funded by EIROforum</a> &nbsp; ISSN 1818-0361</span>
+            <span class="vf-footer__legal-text"><a class="vf-footer__link" href="https://www.eiroforum.org/">Published
+                    and funded by EIROforum</a> &nbsp; ISSN 1818-0361</span>
         </p>
     </div>
 </footer>
