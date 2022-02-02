cce95fb9352fe6cbbec68e8513d2dab164d72687
Szymon Kasprzyk <szymon.kasprzyk@embl.de>
2022-02-02 10:43:49 +0100

    fixes pevious issues display layout

--- dist/wp-content/themes/vf-wp-sis/archive-sis-issue.php
+++ dist/wp-content/themes/vf-wp-sis/archive-sis-issue.php
@@ -4,8 +4,8 @@ get_header();
 <?php include(locate_template('partials/vf-global-header.php', false, false)); ?>
 <?php include(locate_template('partials/vf-navigation.php', false, false)); ?>
 <main class="tmpl-post">
-
-    <section class="vf-hero | vf-u-fullbleed" style="--vf-hero--bg-image: url('/wp-content/themes/vf-wp-sis/assets/images/header/h1-issue.jpg'); --vf-card__image--aspect-ratio 4/5; --vf-hero--bg-image-size: auto 28.5rem">
+    <section class="vf-hero | vf-u-fullbleed"
+        style="--vf-hero--bg-image: url('/wp-content/themes/vf-wp-sis/assets/images/header/h1-issue.jpg'); --vf-card__image--aspect-ratio 4/5; --vf-hero--bg-image-size: auto 28.5rem">
         <div class="vf-hero__content | vf-box | vf-stack vf-stack--400">
             <h2 class="vf-hero__heading">Issue archive
             </h2>
@@ -13,8 +13,9 @@ get_header();
                 all publications dating back to 2006.</p>
         </div>
     </section>
+    <div class="vf-stack vf-stack--800">
 
-            <?php
+        <?php
             $arrayOfDisplayedYears = array();
             if (have_posts()) {
                 while (have_posts()) {
@@ -24,16 +25,16 @@ get_header();
                     $publicationYear = get_the_date('Y');
                     if(!in_array($publicationYear, $arrayOfDisplayedYears)){
                         if(count($arrayOfDisplayedYears) > 0){
-                            echo "</section></div>";
+                            echo '</section></div><hr class="vf-divider">';
                         }
                         $arrayOfDisplayedYears[] = $publicationYear;
                         ?>
-            <div class="embl-grid vf-grid__col-4">
-                <div class="vf-section-header">
-                    <h2 class="vf-section-header__heading"><?php echo $publicationYear; ?></h2>
-                </div>
-                <section class="vf-grid">
-    <?php
+        <div class="vf-grid">
+            <div class="vf-section-header">
+                <h2 class="vf-section-header__heading"><?php echo $publicationYear; ?></h2>
+            </div>
+            <section class="vf-grid vf-grid__col-5">
+                <?php
                     }
                     include(locate_template('partials/vf-issueList-issueTeaser.php', false, false));
 
@@ -41,6 +42,7 @@ get_header();
                 wp_reset_postdata();
             }
             ?>
+        </div>
 
 </main>
 <?php include(locate_template('partials/vf-footer.php', false, false)); ?>
