f7f1661962b93848642ba679703ccdf01cb7a60d
Szymon Kasprzyk <szymon.kasprzyk@embl.de>
2023-02-15 15:30:24 +0100

    allows svg format

--- dist/wp-content/themes/vf-wp-sis/functions.php
+++ dist/wp-content/themes/vf-wp-sis/functions.php
@@ -344,6 +344,40 @@ add_filter( 'posts_search', function( $search, \WP_Query $q )
     return $search;
 }, 10, 2 );
 
+// Allow SVG
+add_filter( 'wp_check_filetype_and_ext', function($data, $file, $filename, $mimes) {
+
+    global $wp_version;
+    if ( $wp_version !== '4.7.1' ) {
+       return $data;
+    }
+  
+    $filetype = wp_check_filetype( $filename, $mimes );
+  
+    return [
+        'ext'             => $filetype['ext'],
+        'type'            => $filetype['type'],
+        'proper_filename' => $data['proper_filename']
+    ];
+  
+  }, 10, 4 );
+  
+  function cc_mime_types( $mimes ){
+    $mimes['svg'] = 'image/svg+xml';
+    return $mimes;
+  }
+  add_filter( 'upload_mimes', 'cc_mime_types' );
+  
+  function fix_svg() {
+    echo '<style type="text/css">
+          .attachment-266x266, .thumbnail img {
+               width: 100% !important;
+               height: auto !important;
+          }
+          </style>';
+  }
+  add_action( 'admin_head', 'fix_svg' );
+
 // removes <p> tag from ACF WYSYWIG field
 // function my_acf_add_local_field_groups() {
 //     remove_filter('acf_the_content', 'wpautop' );
